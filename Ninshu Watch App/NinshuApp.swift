f3b07cb0da8c55071b74a9f953f17f0687a4eda0
Marina Moreira <38846018+MarinaFX@users.noreply.github.com>
2022-12-05 15:42:47 -0300

    feat: finished mvp

---
+++ Ninshu Watch App/NinshuApp.swift
@@ -0,0 +1,17 @@
+//
+//  NinshuApp.swift
+//  Ninshu Watch App
+//
+//  Created by Marina De Pazzi on 04/12/22.
+//
+
+import SwiftUI
+
+@main
+struct Ninshu_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
